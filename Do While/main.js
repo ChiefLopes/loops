cd783a37be9b5193e375b343cc149103039524c0
ChiefLopes <chieflopesekede@gmail.com>
2023-07-19 09:52:06 +0100

    added  definition for do-while loop

---
+++ Do While/main.js
@@ -0,0 +1,7 @@
+//  The do-while loop is similar to the while loop, but with one key difference: it guarantees that the loop's code block is executed at least once, regardless of whether the loop condition is true or false. //
+
+// Here is an example:
+do {
+  // code to be executed
+} while (condition);
+
